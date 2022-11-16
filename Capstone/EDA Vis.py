6ce27c1cb2469e32511d47eaddb710ce6964f284
Peter Soosalu <peter.soosalu@gmail.com>
2022-11-17 09:27:29 +1300

    adding EDA visualization

--- Capstone/EDA Vis.py
+++ Capstone/EDA Vis.py
@@ -3,36 +3,71 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-df = pd.read_csv('https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-DS0701EN-SkillsNetwork/api/dataset_part_2.csv')
+df = pd.read_csv('c:/Users/peters/Downloads/dataset_part_2.csv')
 
 print(df.head(5))
 
 #exploring to see if anything is correlated
 sns.catplot(y="PayloadMass", x="FlightNumber", hue="Class", data=df, aspect = 5)
-plt.xlabel("Flight Number",fontsize=20)
-plt.ylabel("Pay load Mass (kg)",fontsize=20)
+plt.xlabel("Flight Number",fontsize=16)
+plt.ylabel("Pay load Mass (kg)",fontsize=16)
 plt.show()
 
 #1 Plot a scatter point chart with x axis to be Flight Number and y axis to be the launch site, and hue to be the class value
+sns.catplot(y="FlightNumber", x="LaunchSite", hue="Class", data=df, aspect = 5)
+plt.xlabel("Flight Site",fontsize=16)
+plt.ylabel("Flight Number",fontsize=16)
+plt.show()
 
 #2 Plot a scatter point chart with x axis to be Pay Load Mass (kg) and y axis to be the launch site, and hue to be the class value
+sns.catplot(y="PayloadMass",x="LaunchSite",hue ="Class",data=df,aspect= 5)
+plt.xlabel("Launch Site",fontsize=16)
+plt.ylabel("Pay Load Mass (kg)",fontsize=16)
+plt.show()
 
 #3 create a bar chart for the success rate of each orbit
-# HINT use groupby method on Orbit column and get the mean of Class column
+print(df.groupby(['Orbit']).mean()) 
+sns.catplot(x="Orbit",y="Class", kind="bar",data=df)
+plt.xlabel("Orbit",fontsize=16)
+plt.ylabel("Mean",fontsize=16)
+plt.show()
 
 #4 Plot a scatter point chart with x axis to be FlightNumber and y axis to be the Orbit, and hue to be the class value
+sns.scatterplot(x="Orbit",y="FlightNumber",hue="Class",data = df)
+plt.xlabel("Orbit",fontsize=16)
+plt.ylabel("Flight Number",fontsize=16)
+plt.show()
 
 #5 Plot a scatter point chart with x axis to be Payload and y axis to be the Orbit, and hue to be the class value
+sns.scatterplot(x="Orbit",y="PayloadMass",hue="Class",data = df)
+plt.xlabel("Orbit",fontsize=16)
+plt.ylabel("PayloadMass",fontsize=16)
+plt.show()
 
 #6 visualize the launch success yearly trend
-#A function to Extract years from the date 
-year=[]
-def Extract_year(date):
-    for i in df["Date"]:
-        year.append(i.split("-")[0])
-    return year
-
 #Plot a line chart with x axis to be the extracted year and y axis to be the success rate
+year = pd.DatetimeIndex(df['Date']).year
+year = np.array(list(year))
+successratelist = []
+successrate = 0.00
+records = 1
+data = 0
+for x in df['Class']:
+    data = x + data
+    successrate = data/records
+    successratelist.append(successrate)
+    records= records +1
+    
+successratelist = np.array(successratelist)
+d = {'successrate':successratelist,'year':year}
+sns.set(rc={'figure.figsize':(11.7,8.27)})
+sns.lineplot(data=d, x="year", y="successrate" )
+
+plt.xlabel("Year",fontsize=20)
+plt.title('Space X Rocket Success Rates')
+plt.ylabel("Success Rate",fontsize=20)
+plt.show()
+
 
 #----------
 #by now yoyu should obtain some preliminary insights about how each important ariable would affect the success rate,
@@ -43,11 +78,16 @@ features.head()
 #7 Use the function get_dummies and features dataframe to apply OneHotEncoder to the column Orbits, LaunchSite, 
 # LandingPad, and Serial. Assign the value to the variable features_one_hot, display the results using the method
 #head. Your result dataframe must include all features including the encoded ones. 
-# HINT: Use get_dummies() function on the categorical columns
+features_hot = df[['Orbit','LaunchSite','LandingPad','Serial']]
+features_hot['Orbit'] = pd.get_dummies(df['Orbit'])
+features_hot['LaunchSite'] = pd.get_dummies(df['LaunchSite'])
+features_hot['LandingPad'] = pd.get_dummies(df['LandingPad'])
+features_hot['Serial'] = pd.get_dummies(df['Serial'])
+features_hot.head()
 
 #8 Cast all numeric columns to float64
-# HINT: use astype function
+features_hot.astype('float64')
+features_hot.to_csv('dataset_part_3.csv',index=False)
 
-features_one_hot.to_csv('C:\\users\peters\Downloads\dataset_part_3.csv', index=False)
 
 
