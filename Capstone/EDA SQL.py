90627a95ef8b322604de37da9a02147568fc3652
Peter Soosalu <70931473+SailorSoos@users.noreply.github.com>
2022-11-17 19:58:27 +1300

    done for the night

--- Capstone/EDA SQL.py
+++ Capstone/EDA SQL.py
@@ -1,57 +1,49 @@
 #connect to the sqlserver, ie create an instance of the sqldf and query the following:
+import pandasql
+from pandasql import sqldf
 import pandas as pd
 import numpy as np
-import pyodbc
-import sqlserver as ss 
 
-#possibly add adg to the front of sqlserver?
-db = ss.sqlserver('localhost','1433','SpaceX','admin','admin')
+df= pd.read_csv('c://Users/soosa/Downloads/ChicagoCrimeData.csv')
+
+#1 Total number of crimes
+crimeTotal = sqldf("SELECT COUNT(*) FROM ChicagoCrimeData")
+print(crimeTotal)
 
 #1 display the names of the unique launch sites in the space mission
-db.GetRecordsOfColumn('select DISTINCT Launch_Site from tblSpaceX','Launch_Site')
+uniqueSites = sqldf('SELECT DISTINCT Launch_Site FROM df','Launch_Site')
+print(uniqueSites)
 
 #2 display 5 records where launch sites begin with the string 'CCA'
-conn = pyodbc.connect('Driver={SQL Server};'
-                                  'Server=localhost;'
-                                  'Database=SpaceX;'
-                                  'User ID=admin;Password=admin;')
-cursor = conn.cursor()
-
-cursor.execute("select TOP 5 * from tblSpaceX WHERE Launch_Site LIKE 'CCA%'")
-columns = [column[0] for column in cursor.description]
-results = []
-for row in cursor.fetchall():   
-     results.append(dict(zip(columns, row)))
-
-df = pd.DataFrame.from_dict(results)
-print(df)
+CCAsites = sqldf("SELECT TOP 5 * FROM df WHERE Launch_Site LIKE 'CCA%'")
+print(CCAsites)
 
 #3 display the total payload mass carried by booster launched by nasa
-TPM = db.GetRecordsOfColumn("select SUM(PAYLOAD_MASS_KG_) TotalPayloadMass from tblSpaceX where Customer = 'NASA (CRS)'",'TotalPayloadMass')
-ndf= pd.DataFrame(TPM)
-ndf.columns = ['Total Payload Mass']
-print(ndf)
+totalPayloadMass = sqldf("SELECT SUM(PAYLOAD_MASS_KG_) TotalPayloadMass FROM tblSpaceX WHERE Customer = 'NASA (CRS)'",'TotalPayloadMass')
+newdf = pd.DataFrame(totalPayloadMass)
+newdf.columns = ['Total Payload Mass']
+print(newdf)
 
 #4 display average payload mass carried by booster version f9 v1.1
-APM = db.GetRecordsOfColumn("select AVG(PAYLOAD_MASS_KG_) AveragePayloadMass from tblSpaceX where Booster_Version = 'F9 v1.1'",'AveragePayloadMass')
-ndf= pd.DataFrame(APM)
-ndf.columns = ['Average Payload Mass']
-print(ndf)
+avPayloadMass = sqldf("SELECT AVG(PAYLOAD_MASS_KG_) AveragePayloadMass FROM tblSpaceX WHERE Booster_Version = 'F9 v1.1'",'AveragePayloadMass')
+newdf= pd.DataFrame(avPayloadMass)
+newdf.columns = ['Average Payload Mass']
+print(newdf)
 
 #5 list the date when the first successful landing outcome in ground pad was achieved
-SLO = db.GetRecordsOfColumn("select MIN(Date) SLO from tblSpaceX where Landing_Outcome = 'Success (drone ship)'",'SLO')
-ndf= pd.DataFrame(SLO)
-ndf.columns = ['Date which first Successful landing outcome in drone ship was acheived.']
-print(ndf)
+successOutcome = sqldf("SELECT MIN(Date) SLO FROM tblSpaceX WHERE Landing_Outcome = 'Success (drone ship)'",'SLO')
+newdf= pd.DataFrame(successOutcome)
+newdf.columns = ['Date which first Successful landing outcome in drone ship was acheived.']
+print(newdf)
 
 #6 list the names of the booster which have success in drone ship and have payload mass > 4000 & <6000
-SLO = db.GetRecordsOfColumn("select Booster_Version from tblSpaceX where Landing_Outcome = 'Success (ground pad)' AND Payload_MASS_KG_ > 4000 AND Payload_MASS_KG_ < 6000",'Booster_Version')
-ndf= pd.DataFrame(SLO)
-ndf.columns = ['Date which first Successful landing outcome in drone ship was acheived.']
-print(ndf)
+shipMass = sqldf("SELECT Booster_Version FROM tblSpaceX WHERE Landing_Outcome = 'Success (ground pad)' AND Payload_MASS_KG_ > 4000 AND Payload_MASS_KG_ < 6000",'Booster_Version')
+newdf= pd.DataFrame(shipMass)
+newdf.columns = ['Date which first Successful landing outcome in drone ship was acheived.']
+print(newdf)
 
 #7 list the total numer of successful and failure mission outcomes
-cursor.execute("SELECT(SELECT Count(Mission_Outcome) from tblSpaceX where Mission_Outcome LIKE '%Success%') as Successful_Mission_Outcomes,(SELECT Count(Mission_Outcome) from tblSpaceX where Mission_Outcome LIKE '%Failure%') as Failure_Mission_Outcomes")
+successFail = sqldf("SELECT(SELECT Count(Mission_Outcome) FROM tblSpaceX WHERE Mission_Outcome LIKE '%Success%') AS Successful_Mission_Outcomes,(SELECT Count(Mission_Outcome) FROM tblSpaceX where Mission_Outcome LIKE '%Failure%') AS Failure_Mission_Outcomes")
 columns = [column[0] for column in cursor.description]
 results = []
 for row in cursor.fetchall():   
@@ -82,13 +74,9 @@ print(df)
 
 #10 rank the count of landing outcomes (such as failure (drone ship) or success (ground pad)) between the date
 # 2010-06-04 and 2017-03-20, in descending order
-sl = db.GetRecordsOfColumn("SELECT COUNT(Landing_Outcome) AS sl FROM dbo.tblSpaceX WHERE (Landing_Outcome LIKE '%Success%') AND (Date >'04-06-2010') AND (Date < '20-03-2017')",'sl')
-
-ndf= pd.DataFrame(sl)
-ndf.columns = ['Successful Landing Outcomes Between 2010-06-04 and 2017-03-20']
-print(ndf)
-
-
-
+rankOutcomes = sqldf("SELECT COUNT(Landing_Outcome) AS sl FROM dbo.tblSpaceX WHERE (Landing_Outcome LIKE '%Success%') AND (Date >'04-06-2010') AND (Date < '20-03-2017')",'sl')
+newdf= pd.DataFrame(rankOutcomes)
+newdf.columns = ['Successful Landing Outcomes Between 2010-06-04 and 2017-03-20']
+print(newdf)
 
 
